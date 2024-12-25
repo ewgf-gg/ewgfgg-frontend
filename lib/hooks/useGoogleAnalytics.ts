b35a6889c72fadf86ffdc9e3630a55255d2d8d5a
Cedric <77137812+the-beef-calculator@users.noreply.github.com>
2024-12-25 14:09:47 -0500

    added environment variables

--- lib/hooks/useGoogleAnalytics.ts
+++ lib/hooks/useGoogleAnalytics.ts
@@ -7,6 +7,7 @@ export const GA_MEASUREMENT_ID = process.env.GA_MEASUREMENT_ID;
 
 declare global {
   interface Window {
+     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     gtag: (...args: any[]) => void;
   }
 }
