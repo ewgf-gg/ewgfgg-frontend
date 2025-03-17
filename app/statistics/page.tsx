c325dbfc72c38b6fcd06c43ea45cf6e0a216916b
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2025-03-17 16:38:19 -0700

    fixing even MORE caching issues

--- app/statistics/page.tsx
+++ app/statistics/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 export const dynamic = 'force-dynamic';
-export const revalidate = 0;
+export const revalidate = false;
 
 import { Suspense } from 'react';
 import { VersionStatsContent } from '@/components/statistics/VersionStatsContent';
